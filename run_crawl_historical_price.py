f30574ead0fc571134c91f147aabf293366da5e0
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2023-11-25 22:12:32 +0700

    add producer

--- run_crawl_historical_price.py
+++ run_crawl_historical_price.py
@@ -1,32 +1,43 @@
 from datetime import timedelta
 import time
-from tqdm import tqdm
 import os
 
 import pandas as pd
+from tqdm import tqdm
+from kafka import Producer
 
 from crawler.CafeF import HistoricalPriceCafef
 from utils.logger.Logger import Logger
 
+MAX_DAY = 100
+
 date_logger = Logger(r'logs\date_logs\datemapping.log')
 crawler = HistoricalPriceCafef()
 
-
 symbols = pd.read_csv('data/list_companies.csv')['Mã CK▲']
 symbols = ['AAA']
-                        
-for symbol in symbols:
-    for _ in tqdm(range(10)):
+
+# Configure Kafka producer
+producer_config = {
+    'bootstrap.servers': 'localhost:9092',
+}
+
+producer = Producer(producer_config)
+
+kafka_topic = 'historical price'
+for day in tqdm(range(MAX_DAY)):
+    for symbol in symbols:
         data = crawler.getHistoricalPrice(
-            symbol=symbol, 
-            start_date=date_logger.latest_system_time, 
+            symbol=symbol,
+            start_date=date_logger.latest_system_time,
             end_date=date_logger.latest_system_time
         )
-        if os.path.isfile('temp.csv') and os.path.getsize('temp.csv') > 0:
-            data.to_csv('temp.csv', mode='a', index=False, header=False)
-        else:
-            data.to_csv('temp.csv', index=False)
-        date_logger.latest_system_time = date_logger.latest_system_time + timedelta(days=1)
-        time.sleep(5)
 
-date_logger.log_time_system()
+        for _, row in data.iterrows():
+            message_value = row.to_dict()
+            producer.produce(kafka_topic, value=message_value)
+
+    time.sleep(5)
+    date_logger.latest_system_time = date_logger.latest_system_time + timedelta(days=1)
+    date_logger.log_time_system()
+producer.flush()
